f2795727401e3973ec5d8d3e641a520fb44b73a7
Mekhdi <mehdiarsahanov@gmail.com>
2025-04-23 18:10:40 +0400

    create migrations

---
+++ database/migrations/2025_04_23_140705_create_equipment_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('equipment', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('equipment_type_id')
+                ->constrained();
+            $table->string('serial_number');
+            $table->text('note')
+                ->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unique(['equipment_type_id', 'serial_number']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('equipment');
+    }
+};
